6004d92be27f0c94886f8bd4cbd4bbd4a41ad78a
WilliamHuey <wklhuey@gmail.com>
2020-10-08 16:30:50 -0700

    Npm - Add packages: 'ramda'
    Add check for creating html and pdf combination

--- src/commands/build.ts
+++ src/commands/build.ts
@@ -1,6 +1,7 @@
 import { Command, flags } from '@oclif/command'
 import { match, when, __ } from 'ts-pattern'
-import predicate from 'predicate'
+import { isPlainObject } from 'is-what'
+import { intersection } from 'ramda'
 
 export default class Build extends Command {
   static examples = [
@@ -19,6 +20,8 @@ export default class Build extends Command {
 
   static description = `Generate output format of your choosing from these following formats: ${Build.acceptedOutputFormats.join(', ')}`
 
+  static BuildWithOrder = new Map([['htmlPdf', ['html', 'pdf']]]);
+
   private checkAcceptedOutputFormats = () => {
 
   }
@@ -32,11 +35,23 @@ export default class Build extends Command {
       .with(({
         argv: []
       }), () => 'Building - Into all formats')
-      .with(__, (content) => {
+      .with(__, ({ argv }) => {
 
-        console.log(content)
+        const numberArgs = argv.length
 
-        return 'Building - Various'
+        // Check for 'html', 'pdf' or 'pdf', 'html'
+        const htmlWithPdf = Build.BuildWithOrder
+          .get('htmlPdf') || [];
+        const buildIntersection = intersection(argv, htmlWithPdf)
+        const buildIntersectionLen = buildIntersection.length
+        const matchHtmlWithPdf = buildIntersectionLen &&
+          (buildIntersectionLen === numberArgs)
+
+        console.log(matchHtmlWithPdf)
+
+
+
+        return 'Building - Various';
       })
 
       .run()
