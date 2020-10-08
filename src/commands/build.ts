ca6fc14d7a734f8d8b5a9ea234692d3af8acc851
WilliamHuey <wklhuey@gmail.com>
2020-10-08 14:16:18 -0700

    Npm - Add package 'predicate'
    Prepare for general use of the build options

--- src/commands/build.ts
+++ src/commands/build.ts
@@ -1,31 +1,44 @@
 import { Command, flags } from '@oclif/command'
-import { match, __ } from 'ts-pattern'
+import { match, when, __ } from 'ts-pattern'
+import predicate from 'predicate'
 
 export default class Build extends Command {
-  static description = 'Generate output format of your choosing from these following formats: html, pdf, epub'
-
   static examples = [
     '$ convertedbook build pdf',
   ]
 
   static flags = {
-    help: flags.help({ char: 'h' }),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({ char: 'n', description: 'name to print' }),
-    // flag with no value (-f, --force)
-    force: flags.boolean({ char: 'f' }),
+    help: flags.help({ char: 'h' })
   }
 
-  static args = [{ name: 'html' }]
+  static acceptedOutputFormats = ['html', 'pdf', 'epub']
+
+  static args = Build.acceptedOutputFormats.map((format) => {
+    return { name: format }
+  })
+
+  static description = `Generate output format of your choosing from these following formats: ${Build.acceptedOutputFormats.join(', ')}`
+
+  private checkAcceptedOutputFormats = () => {
+
+  }
 
   async run() {
     const buildCmd = this.parse(Build)
 
     const output = match(buildCmd)
+
+      // No build arguments
       .with(({
-        args: { html: 'html' }
-      }), () => 'Building - Html')
-      .with(__, () => 'Building into all formats')
+        argv: []
+      }), () => 'Building - Into all formats')
+      .with(__, (content) => {
+
+        console.log(content)
+
+        return 'Building - Various'
+      })
+
       .run()
 
     this.log(output)
