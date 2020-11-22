f1e16897641b9d95b01f3bd96731799f2a3f5639
WilliamHuey <wklhuey@gmail.com>
2020-11-21 17:59:08 -0800

    Build generate - Allow for multiple file format generation

--- src/functions/build/build-generate.ts
+++ src/functions/build/build-generate.ts
@@ -2,26 +2,18 @@
 import { spawn } from 'child_process';
 
 // Third party modules
-import { bindCallback } from 'rxjs';
+import { bindCallback, forkJoin } from 'rxjs';
 
 // Library modules
 import Build from '../../commands/build';
 import { BuildCheckGoodResults } from './build-checks';
 import { AsyncCheckResults, truncateFilePath } from './build-cli-input-async-checks';
 
-export function buildGenerate(results: BuildCheckGoodResults, asyncResults: AsyncCheckResults): any
-export function buildGenerate(this: Build,
-  results: BuildCheckGoodResults, asyncResults: AsyncCheckResults) {
-  const { conditions } = results,
-    { input, output: outputPath } = conditions.flags,
-    { recognizedFormats } = conditions,
-    { truncateOutput, outputFilename } = asyncResults,
-    normalizedOutputPath = truncateOutput ?
-      `${truncateFilePath(outputPath).filePathFolder}/${outputFilename}` :
-      `${outputPath}${outputFilename}`;
-
+function generateFormat(input: string,
+  normalizedOutputPath: string,
+  format: string) {
   const pandocService = spawn('pandoc',
-    ['-o', `${normalizedOutputPath}.html`, input]);
+    ['-o', `${normalizedOutputPath}.${format}`, input]);
 
   // Convert callback into observable for the
   // 'complete' signal. The observable can also be
@@ -35,14 +27,44 @@ export function buildGenerate(this: Build,
   pandocClose$
     .subscribe({
       next: () => {
-        console.log('Complete generation.');
+        console.log(`Generated ${format}.`);
       },
       error: (e: any) => {
         console.log('Error', e);
       }
     });
 
+  return pandocClose$;
+}
+
+export function buildGenerate(results: BuildCheckGoodResults, asyncResults: AsyncCheckResults): any
+export function buildGenerate(this: Build,
+  results: BuildCheckGoodResults, asyncResults: AsyncCheckResults) {
+  const { conditions } = results,
+    { input, output: outputPath } = conditions.flags,
+    { recognizedFormats } = conditions,
+    { truncateOutput, outputFilename } = asyncResults,
+    normalizedOutputPath = truncateOutput ?
+      `${truncateFilePath(outputPath).filePathFolder}/${outputFilename}` :
+      `${outputPath}${outputFilename}`;
+
+  const generated = recognizedFormats
+    .map((format) => {
+      return generateFormat(input, normalizedOutputPath, format);
+    })
+    .map((pandocClose$) => {
+      return pandocClose$;
+    });
+
+  // Treat the inpout file types as a group even though
+  // one might only be present for easier processing
+  const groupFormatsGenerated$ = forkJoin(generated);
+  groupFormatsGenerated$
+    .subscribe(() => {
+      console.log('Complete format generation.')
+    });
+
   return {
-    pandocClose$
+    pandocClose$: groupFormatsGenerated$
   };
 }
