31202852addaa885525f3c72e5e0bcacdaae86bc
WilliamHuey <wklhuey@gmail.com>
2020-11-22 20:49:04 -0800

    Build Utilities - Use 2 space formatting

--- src/functions/build/build-utilities.ts
+++ src/functions/build/build-utilities.ts
@@ -3,23 +3,23 @@ import { init, head, last } from 'ramda';
 import { isArray } from 'is-what';
 
 export function supposedFileName(fullPath: string) {
-    const fullPathSplit = fullPath.split('/');
-    return last(fullPathSplit)?.split('.');
+  const fullPathSplit = fullPath.split('/');
+  return last(fullPathSplit)?.split('.');
 }
 
 export function getFileNameFromParts(supposeFileParts: string[] | undefined): string |
-    undefined {
-    return isArray(supposeFileParts) ?
-        head(supposeFileParts) : '';
+  undefined {
+  return isArray(supposeFileParts) ?
+    head(supposeFileParts) : '';
 }
 
 export function truncateFilePath(filePath: string) {
-    const filePathSplit = filePath.split('/'),
-        supposeFilePathFolderName = init(filePathSplit),
-        filePathFolder = supposeFilePathFolderName.join('/');
+  const filePathSplit = filePath.split('/'),
+    supposeFilePathFolderName = init(filePathSplit),
+    filePathFolder = supposeFilePathFolderName.join('/');
 
-    return {
-        filePathSplit,
-        filePathFolder
-    };
+  return {
+    filePathSplit,
+    filePathFolder
+  };
 }
