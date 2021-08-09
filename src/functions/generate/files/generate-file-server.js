61e9e51f1a436b15e1329c458beeb833939e8d7a
williamhuey <William Huey>
2021-08-08 17:36:00 -0700

    Generate - Correct the double index.html generation by removing the plugins in the server.js
    Update snowpack version in package.json reference

--- src/functions/generate/files/generate-file-server.js
+++ src/functions/generate/files/generate-file-server.js
@@ -7,10 +7,6 @@ const { loadConfiguration, startServer } = snowpack;
     const configPath = path.resolve(process.cwd(), 'snowpack.config.js');
 
     const config = await loadConfiguration({
-        plugins: [
-            ['./plugins/latex.js', {}],
-            ["@snowpack/plugin-sass"]
-        ],
         devOptions: {
             hmr: true,
             hmrErrorOverlay: false
