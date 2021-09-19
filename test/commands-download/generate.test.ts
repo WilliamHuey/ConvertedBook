3129771250044aec98c8c27b744f6c3f9fba3353
williamhuey <William Huey>
2021-09-18 20:35:44 -0700

    Test - Make generating a folder an observable
    Package.json - Remove the --forbid-only command for proper running of the test cases

--- test/commands-download/generate.test.ts
+++ test/commands-download/generate.test.ts
@@ -1,7 +1,7 @@
 // Third party modules
 import { from } from 'rxjs';
 import { fancy } from 'fancy-test'
-import { filter, share } from 'rxjs/operators';
+import { filter, share, mergeMap } from 'rxjs/operators';
 const del = require('del');
 const isOnline = require('is-online');
 
@@ -19,48 +19,55 @@ describe('Actual Project Generation:', () => {
     del([`${baseTempFolder}*`, `!${baseTempFolder}.gitkeep`]);
   });
 
+  // The generation test relies on internet connectivity to test out
+  // project generation, which provide warning when no connection is found
+  const onLineTest$ = from(isOnline() as Promise<boolean>);
+
+  const isOnLine$ = onLineTest$
+    .pipe(
+      filter((connected): boolean => {
+        return connected;
+      })
+    );
+
+  const isOffLine$ = onLineTest$
+    .pipe(
+      filter((connected): boolean => {
+        return !connected;
+      })
+    );
+
+  isOffLine$
+    .subscribe(() => {
+      console.log('Warning: Did not run tests related to NPM module download due to no internet connectivity');
+    });
+
+  const generatedFolder$ = isOnLine$
+    .pipe(
+      mergeMap(() => {
+        return from(generate.run([`${baseTempFolder}project-generate`, '--npm-project-name', npmProjectName]) as Promise<any>);
+      }),
+      share()
+    );
+
   fancy
     .it('generate project goes to "completion" status', (_, done) => {
+      generatedFolder$
+        .subscribe({
+          next: (result) => {
+            result
+              .projectFolderWithContents$
+              .subscribe({
+                complete: () => {
 
-      // The generation test relies on internet connectivity to test out
-      // project generation, which provide warning when no connection is found
-      const onLineTest$ = from(isOnline() as Promise<boolean>);
-
-      const isOnLine$ = onLineTest$
-        .pipe(
-          filter((connected): boolean => {
-            return connected;
-          })
-        );
-
-      const isOffLine$ = onLineTest$
-        .pipe(
-          filter((connected): boolean => {
-            return !connected;
-          })
-        );
-
-      isOffLine$
-        .subscribe(() => {
-          console.log('Warning: Did not run tests related to NPM module download due to no internet connectivity');
+                  // Able to reach completion is a good sign
+                  // and use this as a marker for a
+                  // successful project generation
+                  done();
+                }
+              })
+          }
         });
+    });
 
-      isOnLine$
-        .subscribe(() => {
-          generate.run([`${baseTempFolder}project-generate`, '--npm-project-name', npmProjectName])
-            .then(res => {
-              res.projectFolderWithContents$
-                .pipe(share())
-                .subscribe({
-                  next: () => {
-                    // Able to reach completion is a good sign
-                    // and use this as a marker for a
-                    // successful project generation
-                    done();
-                  }
-                });
-            });
-
-        });
-    })
 });
