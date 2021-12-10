a505cf90d9c202eb6f1e8ee3968c1aabaa860513
williamhuey <William Huey>
2021-12-09 18:46:06 -0800

    Npm Module - Add package 'port-scout'
    Use random port for build server

--- src/functions/build/build-generate-playwright.ts
+++ src/functions/build/build-generate-playwright.ts
@@ -4,42 +4,43 @@ import * as path from 'path';
 // Third party modules
 import { chromium } from 'playwright';
 import { take } from 'rxjs/operators';
+const scouter = require('port-scout');
 
 // Library modules
 import { BuildGenerate } from './build-generate';
 import { createServer } from './build-server';
 
 interface CreateExactPdf {
-  port: string | Number;
+  port?: string | Number;
   fileName: string;
   outPutFileName: string
 }
 
 // TODO: Read the server port from snowpack dynamically
-
 const snowpackDevServerPort = 8080;
-
-// TODO: Pick a random port which is available
-const simpleServerPort = 9000;
 
 const createExactPdf = ({
   port, fileName, outPutFileName
 }: CreateExactPdf) => {
   const server = createServer({ fileName: `${fileName}.html` });
-  server.listen(simpleServerPort, () => {
-    (async () => {
-      const browser = await chromium.launch();
-      const page = await browser.newPage();
-      await page.goto(`localhost:${port}`);
-      await page.pdf({
-        format: 'A4',
-        printBackground: true,
-        path: `${outPutFileName}.pdf`
-      });
-      await browser.close();
-      server.close();
-    })();
-  });
+  (async () => {
+
+    // Pick a random port which is available
+    const availablePort = await scouter.web();
+    server.listen(availablePort, () => { });
+    const browser = await chromium.launch();
+    const page = await browser.newPage();
+    await page.goto(`localhost:${availablePort}`);
+    await page.pdf({
+      format: 'A4',
+      printBackground: true,
+      path: `${outPutFileName}.pdf`
+    });
+    await browser.close();
+    server.close();
+
+  })();
+
 }
 
 export function playwrightGenerated({
@@ -51,7 +52,6 @@ export function playwrightGenerated({
     .pipe(take(1))
     .subscribe(() => {
       createExactPdf({
-        port: simpleServerPort,
         fileName: path.parse(flags.input).name,
         outPutFileName: normalizedOutputPath
       });
