71bff7366151068124e5bae18793d8346980cd6b
williamhuey <William Huey>
2022-01-06 18:54:05 -0800

    Generate - Add placeholder hmr code to index.js

--- src/functions/generate/files/generate-file-index.js
+++ src/functions/generate/files/generate-file-index.js
@@ -1,2 +1,16 @@
+if (import.meta.hot) {
+
+  // Data restoration upon a completion of reload
+  import.meta.hot.accept(({ }) => {
+
+    // Restore state on the new module.
+
+  })
+
+  // Data to save before a reload
+  import.meta.hot.dispose(() => {
+  })
+}
+
 import "latex.css";
 import "../styles/index.module.scss";
