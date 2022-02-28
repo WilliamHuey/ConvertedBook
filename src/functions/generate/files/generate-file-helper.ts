86fde42780258ec07d6264a5255064df8fa6c71c
williamhuey <William Huey>
2022-02-27 17:27:40 -0800

    Generate - Helper - Add styles for button and the table of contents container

--- src/functions/generate/files/generate-file-helper.ts
+++ src/functions/generate/files/generate-file-helper.ts
@@ -19,11 +19,122 @@ let registerWebComponents = () => {
     static stylesPrefix: string = 'convertedbook';
 
     static styles = css`
-      #${unsafeCSS(ConvertedBookAppBar.stylesPrefix)}-interactions {
-        text-transform: uppercase;
-        top: 0;
+      :host {
+        display: flex;
+        justify-content: end;
+        width: 100%;
+        top: 1rem;
         position: sticky;
         margin-bottom: 1rem;
+        font-family: "JetBrains Mono",monospace;
+        max-width: 100%;
+      }
+
+      #${unsafeCSS(ConvertedBookAppBar.stylesPrefix)}-button {
+        align-items: center;
+        appearance: none;
+        background-color: #FCFCFD;
+        border-radius: 0.25rem;
+        border-width: 0;
+        box-shadow: rgba(45, 35, 66, 0.4) 0 2px 4px,rgba(45, 35, 66, 0.3) 0 7px 13px -3px,#D6D6E7 0 -3px 0 inset;
+        box-sizing: border-box;
+        color: #36395A;
+        cursor: pointer;
+        display: inline-flex;
+        min-height: 2rem;
+        justify-content: center;
+        line-height: 1;
+        list-style: none;
+        padding-left: 1rem;
+        padding-right: 1rem;
+        position: relative;
+        text-align: left;
+        text-decoration: none;
+        transition: box-shadow .15s,transform .15s;
+        user-select: none;
+        -webkit-user-select: none;
+        touch-action: manipulation;
+        white-space: nowrap;
+        will-change: box-shadow,transform;
+        font-size: 0.8rem;
+        font-weight: bold;
+        max-width: 100%;
+        width: 100%;
+        white-space: normal;
+      }
+
+      #${unsafeCSS(ConvertedBookAppBar.stylesPrefix)}-button:before {
+        content: "► ";
+        white-space: pre;
+      }
+
+      #${unsafeCSS(ConvertedBookAppBar.stylesPrefix)}-button.opened:before {
+        content: "▼ ";
+      }
+
+      #${unsafeCSS(ConvertedBookAppBar.stylesPrefix)}-button:focus {
+        box-shadow: #D6D6E7 0 0 0 1.5px inset, rgba(45, 35, 66, 0.4) 0 2px 4px, rgba(45, 35, 66, 0.3) 0 7px 13px -3px, #D6D6E7 0 -3px 0 inset;
+      }
+
+      #${unsafeCSS(ConvertedBookAppBar.stylesPrefix)}-button:hover {
+        box-shadow: rgba(45, 35, 66, 0.4) 0 4px 8px, rgba(45, 35, 66, 0.3) 0 7px 13px -3px, #D6D6E7 0 -3px 0 inset;
+        transform: translateY(-2px);
+      }
+
+      #${unsafeCSS(ConvertedBookAppBar.stylesPrefix)}-button:active {
+        box-shadow: #D6D6E7 0 3px 7px inset;
+        transform: translateY(2px);
+      }
+
+      #${unsafeCSS(ConvertedBookAppBar.stylesPrefix)}-table-of-contents-container {
+        max-height: 45%;
+        font-size: 0.75rem;
+      }
+
+      #${unsafeCSS(ConvertedBookAppBar.stylesPrefix)}-table-of-contents-container nav ul {
+        padding-left: 0;
+      }
+
+      a {
+        background-color: white;
+        border: 1px solid gray;
+        padding: 0.25rem;
+        border-radius: 0.125rem;
+      }
+
+      a,
+      a:visited {
+        text-decoration: none;
+        position: relative;
+        color: #069;
+      }
+
+      a:after,
+      a:visited:after {
+        content: "";
+        height: 3px;
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 0%;
+        background: #069;
+        transition: 0.2s;
+      }
+
+      a:hover:after,
+      a:visited:hover:after {
+        width: 100%;
+      }
+
+      a.block,
+      a:visited.block {
+        display: block;
+        padding: 0.5em;
+      }
+
+      a.block:hover,
+      a:visited.block:hover {
+        background: #eee;
       }
     `;
 
